df310c9b2c044aaa43c57e7a9fa715ae2d9f4dc3
Miha Mlinaric <mlinmiha@gmail.com>
2022-11-26 16:18:29 +0100

    sprememba running

--- project/index.js
+++ project/index.js
@@ -8,8 +8,8 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.use('/users', usersRoutes);
+app.use('https://chess-rpo.herokuapp.com/users', usersRoutes);
 
-app.get('/', (req, res) => res.send('Hello from homepage'));
+app.get('https://chess-rpo.herokuapp.com/', (req, res) => res.send('Hello from homepage'));
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: https://chess-rpo.herokuapp.com/`));
